01561e41a214b9078d2d415a25d593ceecf5ef62
Maysa <maysa.binho.perola@hotmail.com>
2021-10-25 18:25:01 -0300

    Inicio Frontend com ReactTS

---
+++ Blog Pessoal-React/Projeto/blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import'./Home.css';
+
+function Home(){
+return(
+    <>
+        <h1 className="titulo">Home</h1>
+    <img src="https://media.discordapp.net/attachments/882496817550483510/902281765681516644/colorful-planets-chill-scifi-pink-4k-zs-2560x1440.jpg?width=840&height=473" alt="Imagem Tela Inicial" className="img"/>
+
+    </>
+);
+}
+
+export default Home;
